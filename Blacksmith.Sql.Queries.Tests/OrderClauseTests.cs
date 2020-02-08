605f178b617b7dddb62ff92a7a24441787525d0a
hossmi <hossmi@gmail.com>
2020-02-09 00:26:32 +0100

    add tests

---
+++ Blacksmith.Sql.Queries.Tests/OrderClauseTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Blacksmith.Sql.Queries.Tests
+{
+    public class OrderClauseTests
+    {
+        [Fact]
+        public void test2()
+        {
+            Func<string, OrderDirection, OrderClause> createInstance;
+            OrderClause orderClause;
+
+            createInstance = (column, orderDirection) => new OrderClause(column, orderDirection);
+            createInstance
+                .Invoking(c => c(null, (OrderDirection)666))
+                .Should()
+                .Throw<ArgumentNullException>();
+
+        }
+    }
+}
--- Blacksmith.Sql.Queries.Tests/PaginationTests.cs
+++ Blacksmith.Sql.Queries.Tests/PaginationTests.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using System;
 using Xunit;
 
@@ -6,9 +7,51 @@ namespace Blacksmith.Sql.Queries.Tests
     public class PaginationTests
     {
         [Fact]
-        public void Test1()
+        public void default_pagination_instance_tests()
         {
+            Pagination pagination;
 
+            pagination = new Pagination();
+
+            pagination.Should().NotBeNull();
+            pagination.Page.Should().Be(0);
+            pagination.Size.Should().Be(int.MaxValue);
+
+            pagination
+                .Invoking(p => p.Page = -1)
+                .Should()
+                .Throw<ArgumentOutOfRangeException>();
+
+            pagination
+                .Invoking(p => p.Size = 0)
+                .Should()
+                .Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void pagination_cannot_have_negative_page()
+        {
+            Pagination pagination;
+
+            pagination = new Pagination();
+
+            pagination
+                .Invoking(p => p.Page = -1)
+                .Should()
+                .Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void pagination_cannot_have_negative_pageSize()
+        {
+            Pagination pagination;
+
+            pagination = new Pagination();
+
+            pagination
+                .Invoking(p => p.Size = 0)
+                .Should()
+                .Throw<ArgumentOutOfRangeException>();
         }
     }
 }
